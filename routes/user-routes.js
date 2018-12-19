47e1ec3ba7a3885f7440fc904147912ad1d54ba2
Natarajan <vignesh.t.natarajan@accenture.com>
2018-12-19 18:20:12 +0530

    pagination request for get

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/user-model');
 
+/**
+ * Get All user documents
+ */
 router.get('', (request, response) => {
     UserModel.find({}, (err, users) => {
         if (err) {
@@ -12,6 +15,9 @@ router.get('', (request, response) => {
     });
 });
 
+/**
+ * Bulk user document insert
+ */
 router.post('/saveAll', (request, response) => {
     UserModel.insertMany(request.body, (err, users) => {
         if (err) {
@@ -20,6 +26,24 @@ router.post('/saveAll', (request, response) => {
             return response.status(200).send("List of users saved successfully");
         }
     });
+});
+
+/**
+ * Get user based on pagination 
+ */
+router.post('/getUsers/:page', (request, response) => {
+    var perPage = request.body.perPage || 10;
+    var page = request.params.page || 1
+
+    UserModel.find({}).limit(perPage).exec((err, users) => {
+        console.log(users);
+        response.status(200).send(users);
+        // UserModel.count().exec((err, count) => {
+        //     if (err) return next(err);
+        //     var reponse = {users: users, pages: Math.ceil(count / perPage), currentPage: page};
+        //     response.status(200).send(reponse);
+        // })
+    })
 });
 
 module.exports = router;
