210f7991ae2dda1e214e75de6be7d57b1c633368
Natarajan <vignesh.t.natarajan@accenture.com>
2018-12-26 06:44:53 +0530

    user route changes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -29,6 +29,24 @@ router.post('/saveAll', (request, response) => {
 });
 
 /**
+ * Bulk user document insert
+ */
+router.post('/update', (request, response) => {
+    var ObjectId = require('mongodb').ObjectID;
+    var UserIds = request.body.map(function (id) {
+        return id;
+    });
+    UserModel.updateMany({_id: {$in: UserIds}}, {$set: {workPlace: 'Avanade'}}, {multi: true}, (err, docs) => {
+        if (err) {
+            return response.status(400).send(err);
+        } else {
+            return response.status(200).send({isSuccess: true});
+        }
+    });
+});
+
+
+/**
  * Get user based on pagination 
  */
 router.post('/getUsers/:page', (request, response) => {
@@ -36,7 +54,7 @@ router.post('/getUsers/:page', (request, response) => {
     // var workSpaceType = request.body.workSpaceType;
     var page = parseInt(request.params.page) || 1
     // var query = workSpaceType ? {'workPlace': workSpaceType} : {};
-    UserModel.find({}).skip((perPage * page) - perPage).limit(perPage).exec((err, users) => {
+    UserModel.find({}).limit(perPage * page).exec((err, users) => {
         UserModel.countDocuments().exec((err, count) => {
             if (err) return next(err);
             var reponse = {users: users, pages: Math.ceil(count / perPage), currentPage: page};
